59b10fd1dcddb6946d5e4b065402323e5fa1e861
leewillis77 <leewillis77@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-05 13:49:09 +0000

    Tagging 1.0
    git-svn-id: https://plugins.svn.wordpress.org/wp-e-commerce-show-personalisation/tags/1.0@459655 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ wpec_show_personalisation.php
@@ -0,0 +1,60 @@
+<?php
+/*
+Plugin Name: WP e-Commerce Show Personlisation
+Plugin URI: http://www.leewillis.co.uk/wordpress-plugins/?utm_source=wordpress&utm_medium=www&utm_campaign=wpec-show-personalisation
+Description: A simple plugin that shows the personlisation information entered by users in the cart widget, and during checkout.
+Author: Lee Willis
+Version: 1.0
+Author URI: http://www.leewillis.co.uk/
+License: GPLv3
+*/
+
+if ( ! is_admin() ) {
+
+    class wpec_show_personalisation {
+
+
+
+	    function __construct() {
+
+		    // Show the personlisation information during checkout
+		    add_action ( 'wpsc_after_checkout_cart_item_name', array ( &$this, 'checkout_personalisation_information' ) );
+		    add_action ( 'wpsc_after_cart_widget_item_name', array ( &$this, 'cart_widget_personalisation_information' ) );
+    
+	    }
+
+
+
+	    public function checkout_personalisation_information() {
+		    $this->show_personalisation_information ( 'checkout' );
+	    }
+
+
+
+	    public function cart_widget_personalisation_information() {
+		    $this->show_personalisation_information ( 'cart_widget' );
+	    }
+
+
+
+
+	    public function show_personalisation_information($context) {
+
+		    global $wpsc_cart; 
+    
+		    // Seperate out the options into individual items
+		    $info = $wpsc_cart->cart_item->custom_message;
+    
+		    echo apply_filters ( 'wpec_showp_before_info', '<br/><span class="wpec_showp_'.esc_attr ( $context ).'_text">', $context );
+
+			echo nl2br ( esc_html ( apply_filters ( 'wpec_showp_personalisation_info', $info,  $context ) ) );
+    
+		    echo apply_filters ( 'wpec_showp_after_info', '</span>', $context );
+    
+	    }
+    
+    }
+    
+    $wpec_show_personalisation = new wpec_show_personalisation();
+    
+}
